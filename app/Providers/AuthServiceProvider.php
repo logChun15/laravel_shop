6b297d0ba6707963d3e98ee5acd69f738b0fa970
logChun15 <luochun15@gmail.com>
2022-10-11 12:45:18 +0000

    修改和删除收货地址

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        UserAddress::class => UserAddressPolicy::class,
     ];
 
     /**
