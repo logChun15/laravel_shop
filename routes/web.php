09b94012f2af14924e5fcc0c539b8dd57be11c31
logChun15 <luochun15@gmail.com>
2022-10-12 12:54:30 +0000

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', 'PagesController@root')->name('root');
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 // Auth::routes();
 Auth::routes(['verify' => true]);
 
