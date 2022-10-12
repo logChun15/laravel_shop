90fff0472435ed4ca6a4498a26dcc2c64c0f74e0
logChun15 <luochun15@gmail.com>
2022-10-12 11:23:39 +0000

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+// Route::get('/', 'PagesController@root')->name('root');
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 
 // Auth::routes();
 Auth::routes(['verify' => true]);
@@ -30,7 +32,7 @@ Route::group(['middleware' => 'auth'], function() {
         Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
         Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
         Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
-        Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+        Route::delete('user_addresses/{user_address}', ' @destroy')->name('user_addresses.destroy');
     });
 
 });
