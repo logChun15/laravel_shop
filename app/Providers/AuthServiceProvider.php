46a70125ce4cb08fd528727a014927550fc447c9
logChun15 <luochun15@gmail.com>
2022-10-14 10:09:53 +0000

    用户界面 - 订单详情页

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -17,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
         UserAddress::class => UserAddressPolicy::class,
+        Order::class => OrderPolicy::class,
     ];
 
     /**
